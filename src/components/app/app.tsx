9fff37c71cb1a045492295d2fd2423490338fe51
Alexandr <kotte48@gmail.com>
2024-09-30 09:52:26 +0300

    Нарезка компонентов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,18 @@
-import MainPage from "../../pages/main-page/main-page";
+import MainPage from '../../pages/main-page/main-page';
 
-function App(): JSX.Element {
-  return (
-    <>
-      <MainPage />
-    </>
-  );
+type FilmPromoInfo = {
+  titlePromo: string;
+  genrePromo: string;
+  yearPromo: string;
+}
+
+type AppProps = {
+  filmsCount: number;
+  filmPromoInfo: FilmPromoInfo;
+}
+
+function App({ filmsCount, filmPromoInfo }: AppProps): JSX.Element {
+  return <MainPage filmsCount={filmsCount} filmPromoInfo={filmPromoInfo} />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const FILM_COUNT = 20;
+
+type FilmPromoInfo = {
+  titlePromo: string;
+  genrePromo: string;
+  yearPromo: string;
+}
+
+const FILM_PROMO_INFO: FilmPromoInfo = {
+  titlePromo: 'The Grand Budapest Hotel',
+  genrePromo: 'Drama',
+  yearPromo: '2014',
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App filmsCount={FILM_COUNT} filmPromoInfo={FILM_PROMO_INFO} />
   </React.StrictMode>,
 );
